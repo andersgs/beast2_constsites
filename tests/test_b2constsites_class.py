7ac8638ac00af255023aca6420d386fbf4722461
Anders Goncalves da Silva <andersgs@gmail.com>
2017-10-03 21:31:22 +1100

    Added tests to test XML parsing and rendering code

--- tests/test_b2constsites_class.py
+++ tests/test_b2constsites_class.py
@@ -7,6 +7,7 @@ test_folder = os.path.dirname(os.path.realpath(__file__))
 test_seq = os.path.join(test_folder, 'test.fa')
 test_vcf = os.path.join(test_folder, 'test.vcf')
 test_bed = os.path.join(test_folder, 'test.bed')
+test_xml = os.path.join(test_folder, 'test.xml')
 
 test_seq_withN = os.path.join(test_folder, 'test_withN.fa')
 test_seq_withDash = os.path.join(test_folder, 'test_withdash.fa')
@@ -14,25 +15,33 @@ test_seq_withDash = os.path.join(test_folder, 'test_withdash.fa')
 b2cs_bed = B2ConstSites(seq=test_seq,
                         vcf=test_vcf,
                         maskbed=test_bed,
+                        xml=test_xml,
                         fmt='fasta')
-
 
 b2cs_nobed = B2ConstSites(seq=test_seq,
                           vcf=test_vcf,
+                          xml=test_xml,
                           fmt='fasta')
 
 b2cs_withN = B2ConstSites(seq=test_seq_withN,
                           vcf=test_vcf,
+                          xml=test_xml,
                           fmt='fasta')
 
 b2cs_withDash = B2ConstSites(seq=test_seq_withDash,
                              vcf=test_vcf,
+                             xml=test_xml,
                              fmt='fasta')
 
 
 def test_load_seq_with_bed():
     b2cs_bed.load_seq()
     assert len(b2cs_bed.seqrec) == 16
+
+
+def test_load_xml_with_bed():
+    b2cs_bed.load_xml()
+    assert b2cs_bed.data_index == 1
 
 
 def test_load_vcf_with_bed():
@@ -63,6 +72,22 @@ def test_const_sites_C_with_bed():
 def test_const_sites_G_with_bed():
     b2cs_bed.const_sites()
     assert 4 == b2cs_bed.tally['G']
+
+
+def test_rename_original_data_tag():
+    b2cs_bed.rename_original_data_tag()
+    assert b2cs_bed.data_el_id == "alignment"
+    assert b2cs_bed.new_data_id == "alignmentOriginal"
+
+
+def test_create_new_xml(tmpdir):
+    p = tmpdir.mkdir('new_xml')
+    xml_file = os.path.basename(b2cs_bed.xml)
+    xml_path = p.join(xml_file)
+    b2cs_bed.xml = xml_path.realpath()
+    print(xml_path.realpath())
+    b2cs_bed.create_new_xml()
+    assert os.path.exists(b2cs_bed.new_xml)
 
 
 def test_output_with_bed(capfd):

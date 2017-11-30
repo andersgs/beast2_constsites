50771c647f30ad5c7b69c21f6f63284e174e7296
Anders Goncalves da Silva <andersgs@gmail.com>
2017-11-30 13:45:31 +1100

    Added support for iqtree.

--- b2constsites/run_b2constsites.py
+++ b2constsites/run_b2constsites.py
@@ -1,37 +1,78 @@
 import click
 import logging
-from .b2constsites import B2ConstSites
+from b2constsites import B2
+from b2constsites import IQtree
 
 
 @click.command()
+@click.argument('tool', type=click.Choice(['beast2', 'iqtree']),
+                default='beast2')
 @click.argument('seq')
 @click.argument('vcf')
-@click.argument('xml')
+@click.option('-x', '--xml', default=None,
+              help='An XML file produced with BEAUTi')
 @click.option('-m', '--maskbed', default=None,
               help='A BED file with positions to mask')
 @click.option('-f', '--fmt', default='fasta',
               help='seq sequence format', show_default=True)
-def run_b2constsites(seq, vcf, xml, maskbed, fmt):
+def run_b2constsites(tool, seq, vcf, xml, maskbed, fmt):
+    '''
+    Welcome to Beast2 Constant Sites.
+
+    This version supports output for BEAST2 and IQTree.
+
+    BEAST2 example:
+
+        run_b2cs -x myxml.xml beast2 myref.fasta myvar.vcf
+
+        NOTE: If running BEAST2, an XML file is **REQUIRED**.
+
+    IQTree example:
+
+        run_b2cs iqtree myref.fasta myvar.vcf
+
+    If you encounter any issues, please submit them through:
+
+        https://github.com/andersgs/beast2_constsites/issues
+    '''
     logging.basicConfig(
         level=logging.INFO,
         format='%(asctime)s B2CONSTSITES %(levelname)-5s %(message)s',
         datefmt='%Y-%m-%d %H:%M')
-    b2cs = B2ConstSites(seq=seq,
-                        vcf=vcf,
-                        xml=xml,
-                        maskbed=maskbed,
-                        fmt=fmt)
-    b2cs.load_seq()
-    b2cs.load_xml()
-    b2cs.rename_original_data_tag()
-    b2cs.load_vcf()
-    b2cs.load_mask()
-    b2cs.const_sites()
-    b2cs.create_new_xml()
-    logging.info(f'Created {b2cs.new_xml}.')
-    logging.info(f'Use: beast <options> {b2cs.new_xml}')
-    logging.info('DONE!')
-    pass
+    if tool == 'beast2':
+        if xml is None:
+            raise IOError("When using beast2 please provide an XML file.")
+        cs = B2(seq=seq,
+                vcf=vcf,
+                xml=xml,
+                maskbed=maskbed,
+                fmt=fmt)
+        cs.load_seq()
+        cs.load_xml()
+        cs.rename_original_data_tag()
+        cs.load_vcf()
+        cs.load_mask()
+        cs.const_sites()
+        cs.create_new_xml()
+        logging.info(f'Created {b2cs.new_xml}.')
+        logging.info(f'Use: beast <options> {b2cs.new_xml}')
+        logging.info('DONE!')
+    elif tool == 'iqtree':
+        cs = IQtree(seq=seq,
+                    vcf=vcf,
+                    maskbed=maskbed,
+                    fmt=fmt)
+        cs.load_seq()
+        cs.load_vcf()
+        cs.load_mask()
+        cs.const_sites()
+        print(cs)
+    else:
+        logging.warning(f'I don\'t yet support {tool}.')
+        logging.warning('Please file an issue on '
+                        'https://github.com/andergs/beast2_constsites/issues')
+        logging.warning('Exiting without doing anything!')
+
 
 
 if __name__ == '__main__':
